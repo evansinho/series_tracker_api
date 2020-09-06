9cb3417186dd6ce1e8fbd2e44feeea724c7cdb4d
evanson <eigiri@uplandsoftware.com>
2020-09-06 21:41:07 +0100

    create user registration route and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authorized, only: [:auto_login]
+   # REGISTER
+   def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: @user.errors.full_messages}
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
 end
