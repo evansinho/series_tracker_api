8728497c8e217b678ad72af9122b0c6eb4cf3b76
evanson <eigiri@uplandsoftware.com>
2020-09-07 20:29:13 +0100

    create the movie model and associations

---
+++ db/migrate/20200907191850_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.integer :episode
+      t.integer :episode_goal
+      t.integer :season
+      t.integer :season_goal
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_192613) do
+ActiveRecord::Schema.define(version: 2020_09_07_191850) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.integer "episode"
+    t.integer "episode_goal"
+    t.integer "season"
+    t.integer "season_goal"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_09_06_192613) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "movies", "users"
 end
