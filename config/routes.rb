034e0a67cf8870b06077f383ce1c56aa8652f52b
evanson <eigiri@uplandsoftware.com>
2020-09-07 20:52:17 +0100

    create routes for movies

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'movies/index'
+  post 'movies/create'
+  get '/show/:id', to: 'movies#show'
+  put '/update/:id', to: 'movies#update'
+  delete '/destroy/:id', to: 'movies#destroy'
+  # get 'movies/progress'
   post "/signup", to: "users#create"
   post "/login", to: "users#login"
 end
