5d4aa2136d5e7539ff12135e24e943be50662757
evanson <eigiri@uplandsoftware.com>
2020-09-21 09:53:42 +0100

    fix linter error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   end
 
   def auto_login
-    if @user
+    if @user # rubocop:todo Style/GuardClause
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, token: token }
     end
